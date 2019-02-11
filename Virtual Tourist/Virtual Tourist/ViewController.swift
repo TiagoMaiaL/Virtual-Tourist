54643c0f35b4023c8614ffbc2fe807d860552917
Tiago Maia <tiago.maia.lopes@gmail.com>
2019-02-11 19:38:48 -0200

    Add initial one-page app project

---
+++ Virtual Tourist/Virtual Tourist/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Virtual Tourist
+//
+//  Created by Tiago Maia Lopes on 11/02/19.
+//  Copyright © 2019 Tiago Maia Lopes. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
---
+++ Virtual Tourist/Virtual TouristTests/Virtual_TouristTests.swift
@@ -0,0 +1,34 @@
+//
+//  Virtual_TouristTests.swift
+//  Virtual TouristTests
+//
+//  Created by Tiago Maia Lopes on 11/02/19.
+//  Copyright © 2019 Tiago Maia Lopes. All rights reserved.
+//
+
+import XCTest
+@testable import Virtual_Tourist
+
+class Virtual_TouristTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
