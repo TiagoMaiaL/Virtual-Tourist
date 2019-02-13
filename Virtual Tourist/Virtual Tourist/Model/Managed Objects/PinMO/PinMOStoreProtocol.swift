442939e8cbf9559d6e6fb3bb41cac5a35a7d3447
Tiago Maia <tiago.maia.lopes@gmail.com>
2019-02-13 12:53:57 -0200

    Add folder structure for the stores

---
+++ Virtual Tourist/Virtual Tourist/Model/Managed Objects/PinMO/PinMOStoreProtocol.swift
@@ -0,0 +1,9 @@
+//
+//  PinMOStoreProtocol.swift
+//  Virtual Tourist
+//
+//  Created by Tiago Maia Lopes on 13/02/19.
+//  Copyright © 2019 Tiago Maia Lopes. All rights reserved.
+//
+
+import Foundation
